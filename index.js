c793110687b48c3a97fdc56e68e055ab3d4e0046
Anubhav311 <anubhavbhambri1@gmail.com>
2019-09-15 22:53:11 +0530

    created server on port 5000

---
+++ index.js
@@ -0,0 +1,5 @@
+const server = require('./config/server');
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => console.log(`\n\n \u{1F680}\u{1F680} Express API Running on Port ${port} \n\n`))
